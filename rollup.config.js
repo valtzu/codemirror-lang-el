1cdb201c470e3e3c03bccd1da39f5f5130c17a7b
valtzu <valtzu@gmail.com>
2024-04-07 16:48:21 +0300

    Create sourcemap in non-ci envs

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 import typescript from "rollup-plugin-ts"
-import {lezer} from "@lezer/generator/rollup"
+import { lezer } from "@lezer/generator/rollup"
+
+const sourcemap = !process.env.CI;
 
 export default {
   input: "src/index.ts",
-  external: id => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
+  external: id => id !== "tslib" && !/^(\.?\/|\w:)/.test(id),
   output: [
-    {file: "dist/index.cjs", format: "cjs"},
-    {dir: "./dist", format: "es"}
+    { file: "dist/index.cjs", format: "cjs", sourcemap },
+    { dir: "./dist", format: "es", sourcemap }
   ],
   plugins: [lezer(), typescript()]
 }
